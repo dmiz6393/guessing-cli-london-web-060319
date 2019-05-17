b2438f6b8e0cc78d7b654808d487c44c89ce76cf
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-17 15:40:12 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,15 +5,16 @@ def run_guessing_game
   num = rand(1..6) 
   if response=="exit" 
     puts "Goodbye!"
-    
 elsif
-reponse.to_i==num 
+response.to_i==num 
 puts "You guessed the correct number!"
 
-elseif response.to_i!=num
+elsif response.to_i!=num
   puts "The computer guessed #{num}."
+  
 else puts "Invalid input" 
   
+end 
 end 
 end
 end 
