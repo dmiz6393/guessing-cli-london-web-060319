b3f2dcd9ad441116fb3fe8d8cc594b27c9194ade
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-17 15:46:18 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   response=gets.chomp
   num=rand(1..6) 
   
-  while response!="exit"
+  while response!="exit"do 
   
   do if response.to_i== num 
   puts "You guessed the correct number!"
